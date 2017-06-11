1e11a735fb44c1b5df7c713e96bf2c5fafa5de41
Baelyk <b43lyk@gmail.com>
2017-06-10 19:22:37 -0500

    Nation Info and Search Wiki

--- TODO.md
+++ TODO.md
@@ -4,3 +4,7 @@
 -   Keep points in Drive too?
 -   Search the RIA wiki
 -   NationStates info (delegate, # nations in region, etc)
+-   `nation` command where it links to the ns page and or wiki page
+    -   `nation link` link to the ns nation
+    -   `nation wiki` link to the wiki page on the nation
+    -   `nation overview` show the flag and give an overview (from wiki or ns)
--- main.js
+++ main.js
@@ -33,24 +33,159 @@ const commands = [
       return args[0] === 'ns'
     },
     function: nsApi
+  }, {
+    name: 'Nation Info',
+    invoker: (args) => {
+      return args[0] === 'nation'
+    },
+    function: nationInfo
   }
 ]
+
+function nationInfo (message, args) {
+  // ['nation', 'link or wiki or overview', 'nation']
+  if (args[1] === 'link') {
+    args[2] = args.splice(2).join('_')
+    if (args[2].length > 0) {
+      ns.nationRequest(args[2], ['name'])
+      .then(data => {
+        message.reply(`https://nationstates.net/nation=${args[2]}`)
+      })
+      .catch(error => {
+        if (error.responseMetadata.statusMessage === 'Not Found') {
+          message.reply(`I couldn't find a nation called '${args[2]}'`)
+        } else {
+          message.reply(`that gave me an unexpected error!`)
+        }
+      })
+    } else {
+      message.react('😒')
+      message.reply('the full syntax includes a nation, like so: `nation link <nation_name_here>`')
+    }
+  } else if (args[1] === 'wiki') {
+    args[2] = args.splice(2).join(' ')
+    if (args[2].length > 0) {
+      wiki.api.call({
+        action: 'query',
+        list: 'search',
+        srsearch: args[2],
+        srwhat: 'text',
+        utf8: ''
+      }, (err, info, next, data) => {
+        if (err) console.error(err)
+        let result = data.query.search[0]
+        if (result !== undefined) {
+          const url = `http://ria.baelyk.com/wiki/index.php?title=${result.title.replace(' ', '_')}`
+          message.reply(url)
+        } else {
+          message.reply(`hmmm... I didn't get any results for '${args[2]}'`)
+        }
+      })
+    } else {
+      message.react('😒')
+      message.reply('the full syntax includes a nation, like so: `nation wiki <nation_name_here>`')
+    }
+  } else if (args[1] === 'overview') {
+    if (args[2].length > 0) {
+      wiki.api.call({
+        action: 'query',
+        list: 'search',
+        srsearch: args[2],
+        srwhat: 'text',
+        utf8: ''
+      }, (err, info, next, data) => {
+        if (err) console.error(err)
+        if (data.query.search[0] !== undefined) {
+          let title = data.query.search[0].title
+          if (title !== undefined) {
+            wiki.api.call({
+              action: 'query',
+              prop: 'extracts',
+              exintro: '',
+              explaintext: '',
+              titles: escape(title),
+              redirects: 1,
+              indexpageids: 1
+            }, (err, info, next, data) => {
+              if (err) console.error(err)
+              let summary = data.query.pages[data.query.pageids[0]].extract
+              if (summary !== undefined) {
+                summary = summary === '' ? 'no summary available' : summary
+                message.reply(summary)
+              } else {
+                message.reply(`hmmm... I didn't get any results for '${args[2]}'`)
+              }
+            })
+          } else {
+            message.reply(`hmmm... I didn't get any results for '${args[2]}'`)
+          }
+        } else {
+          message.reply(`hmmm... I didn't get any results for '${args[2]}'`)
+        }
+      })
+    } else {
+      message.react('😒')
+      message.reply('the full syntax includes a nation, like so: `nation overview <nation_name_here>`')
+    }
+  }
+}
 
 function nsApi (message, args) {
   // args = ['ns', 'delegate', 'region']
   args[1] = args[1].toLowerCase()
   args[2] = args.splice(2).join('_')
-  if (args[1] === 'delegate' || args[1] === 'founder' || args[1] === 'numnations') {
-    ns.regionRequest(args[2], [args[1]]).then(data => {
-      let info = data[args[1]]
-      message.reply(`the ${args[1]} of ${args[2]} is ${info}`)
-    })
+  if (args[1] === 'delegate' || args[1] === 'founder' || args[1] === 'numnations' || args[1] === 'rflag') {
+    if (args[1] === 'rflag') {
+      ns.regionRequest(args[2], ['flag']).then(data => {
+        let info = data.flag
+        message.reply({file: info})
+      })
+      .catch(error => {
+        if (error.responseMetadata.statusMessage === 'Not Found') {
+          message.reply(`I couldn't find a region called '${args[2]}'`)
+        } else {
+          message.reply(`that gave me an unexpected error!`)
+        }
+      })
+    } else {
+      ns.regionRequest(args[2], [args[1]]).then(data => {
+        let info = data[args[1]]
+        message.reply(`the ${args[1]} of ${args[2]} is ${info}`)
+      })
+      .catch(error => {
+        if (error.responseMetadata.statusMessage === 'Not Found') {
+          message.reply(`I couldn't find a region called '${args[2]}'`)
+        } else {
+          message.reply(`that gave me an unexpected error!`)
+        }
+      })
+    }
   } else if (args[1] === 'animal' || args[1] === 'capital' || args[1] === 'currency' || args[1] === 'demonym' || args[1] === 'endorsements' || args[1] === 'flag' || args[1] === 'fullname' || args[1] === 'gdp' || args[1] === 'influence' || args[1] === 'leader' || args[1] === 'motto' || args[1] === 'population' || args[1] === 'region' || args[1] === 'religion' || args[1] === 'tax') {
-    ns.nationRequest(args[2], [args[1]]).then(data => {
-      console.log(data)
-      let info = data[args[1]]
-      message.reply(`the ${args[1]} of ${args[2]} is ${info}`)
-    })
+    if (args[1] === 'flag') {
+      ns.nationRequest(args[2], ['flag']).then(data => {
+        let info = data.flag
+        message.reply({file: info})
+      })
+      .catch(error => {
+        if (error.responseMetadata.statusMessage === 'Not Found') {
+          message.reply(`I couldn't find a nation called '${args[2]}'`)
+        } else {
+          message.reply(`that gave me an unexpected error!`)
+        }
+      })
+    } else {
+      ns.nationRequest(args[2], [args[1]]).then(data => {
+        let info = data[args[1]]
+        message.reply(`the ${args[1]} of ${args[2]} is ${info}`)
+      })
+      .catch(error => {
+        if (error.responseMetadata.statusMessage === 'Not Found') {
+          message.reply(`I couldn't find a nation called '${args[2]}'`)
+        } else {
+          message.reply(`that gave me an unexpected error!`)
+        }
+      })
+    }
   }
 }
 
@@ -73,6 +208,10 @@ function points (message, args) {
       message.reply(`gave ${nickname} ${args[3]} points.`)
       saveData()
     })
+    .catch((error) => {
+      if (error) console.error(error)
+      if (error.code === 50035) message.reply('make sure you ping the person you want to give points to.')
+    })
   } else if (args[1] === 'view' && args.length === 3) {
     let nickname
     client.fetchUser(parseId(args[2])).then(user => {
@@ -85,28 +224,32 @@ function points (message, args) {
       }
       saveData()
     })
+    .catch((error) => {
+      if (error) console.error(error)
+      if (error.code === 50035) message.reply('make sure you ping the person you want to view the points of.')
+    })
   } else {
     message.reply('What do you want to do with that command? Use `points give <user> <points>` to give someone points, or use `points view <user>` to view someones points!')
   }
 }
 
 function searchWiki (message, args) {
-  // wiki.search('Babylati', (err, results) => {
-  //   if (err) console.error(err)
-  //   console.log(results)
-  // })
+  let search = args.slice(1).join(' ')
   wiki.api.call({
     action: 'query',
     list: 'search',
-    srsearch: args.slice(1).join(' '),
+    srsearch: search,
     srwhat: 'text',
     utf8: ''
   }, (err, info, next, data) => {
-    console.log(data.query.search)
     if (err) console.error(err)
-    let result = data.query.search[0]
-    const url = `http://ria.baelyk.com/wiki/index.php?title=${result.title.replace(' ', '_')}`
-    message.reply(`page ${result.title} found: ${url}`)
+    if (data.query.search[0] !== undefined) {
+      let result = data.query.search[0]
+      const url = `http://ria.baelyk.com/wiki/index.php?title=${result.title.replace(' ', '_')}`
+      message.reply(`page ${result.title} found: ${url}`)
+    } else {
+      message.reply(`hmmm... I didn't get any results for '${search}'`)
+    }
   })
 }
 
@@ -130,6 +273,7 @@ client.on('message', message => {
     commands.forEach(command => {
       if (command.invoker(args)) {
         doneCommand = true
+        console.log(`${message.author} issued command ${command.name} with args [${args}] and message content ${message.content}`)
         command.function(message, args)
       }
     })
